ea651d8d3f1dbf862fbe790bc9f2825aa1ceb019
agegeekokou <delaliage@yahoo.fr>
2024-09-10 06:23:09 +0200

    Update action method implemented

--- Bloggie.Web/Controllers/AdminBlogPostsController.cs
+++ Bloggie.Web/Controllers/AdminBlogPostsController.cs
@@ -53,7 +53,7 @@ namespace Bloggie.Web.Controllers
             };
 
             // Map tags from selected tags
-            var selectedTag = new List<Tag>();
+            var selectedTags = new List<Tag>();
 
             foreach (var selectedTagId in addBlogPostRequest.SelectedTags)
             {
@@ -62,12 +62,12 @@ namespace Bloggie.Web.Controllers
 
                 if (existingTag != null)
                 {
-                    selectedTag.Add(existingTag);
+                    selectedTags.Add(existingTag);
                 }
             }
 
             // Mapping tags back to domain model
-            blogPost.Tags = selectedTag;
+            blogPost.Tags = selectedTags;
 
             await blogPostsRepository.AddAsync(blogPost);
 
@@ -80,6 +80,92 @@ namespace Bloggie.Web.Controllers
             var blogPosts = await blogPostsRepository.GetAllAsync();
 
             return View(blogPosts);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(Guid id)
+        {
+            // Retrieve the result from the repository
+            var blogPost = await blogPostsRepository.GetAsync(id);
+            var tagDomainModel = await tagRepository.GetAllAsync();
+
+            if (blogPost != null)
+            {
+                // Map the domain model into the view model
+                var model = new EditBlogPostRequest
+                {
+                    Id = blogPost.Id,
+                    Heading = blogPost.Heading,
+                    PageTitle = blogPost.PageTitle,
+                    Content = blogPost.Content,
+                    ShortDescription = blogPost.ShortDescription,
+                    FeaturedImageUrl = blogPost.FeaturedImageUrl,
+                    UrlHandle = blogPost.UrlHandle,
+                    PublishedDate = blogPost.PublishedDate,
+                    Author = blogPost.Author,
+                    Visible = blogPost.Visible,
+                    Tags = tagDomainModel.Select(x => new SelectListItem
+                    {
+                        Text = x.Name,
+                        Value = x.Id.ToString()
+                    }),
+                    SelectedTags = blogPost.Tags.Select(x => x.Id.ToString()).ToArray()
+                };
+
+                return View(model);
+            }
+
+            // Pass data to view
+            return View(null);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(EditBlogPostRequest editBlogPostRequest)
+        {
+            // Map view model back to domain model
+            var blogPostDomainModel = new BlogPost
+            {
+                Id = editBlogPostRequest.Id,
+                Heading = editBlogPostRequest.Heading,
+                PageTitle = editBlogPostRequest.PageTitle,
+                Content = editBlogPostRequest.Content,
+                ShortDescription = editBlogPostRequest.ShortDescription,
+                FeaturedImageUrl = editBlogPostRequest.FeaturedImageUrl,
+                UrlHandle = editBlogPostRequest.UrlHandle,
+                PublishedDate = editBlogPostRequest.PublishedDate,
+                Author = editBlogPostRequest.Author,
+                Visible = editBlogPostRequest.Visible,
+            };
+
+            // Map tags into domain model
+            var selectedTags = new List<Tag>();
+
+            foreach (var selectedTag in editBlogPostRequest.SelectedTags)
+            {
+                if (Guid.TryParse(selectedTag, out var tag))
+                {
+                    var foundTag = await tagRepository.GetAsync(tag);
+
+                    if (foundTag != null)
+                    {
+                        selectedTags.Add(foundTag);
+                    }
+                }
+            }
+
+            blogPostDomainModel.Tags = selectedTags;
+
+            // Submit information to repository to update
+            var updatedBlog = await blogPostsRepository.UpdateAsync(blogPostDomainModel);
+
+            if (updatedBlog != null)
+            {
+                // Show success notification
+                return RedirectToAction("Edit");   
+            }
+
+            // Show error notification
+            return RedirectToAction("Edit");
         }
     }
 }
--- Bloggie.Web/Repositories/BlogPostsRepository.cs
+++ Bloggie.Web/Repositories/BlogPostsRepository.cs
@@ -22,17 +22,39 @@ namespace Bloggie.Web.Repositories
 
         public async Task<IEnumerable<BlogPost>> GetAllAsync()
         {
-            return await bloggieDbContext.BlogPosts.ToListAsync();
+            return await bloggieDbContext.BlogPosts.Include(x => x.Tags).ToListAsync();
         }
 
-        public Task<BlogPost?> GetAsync(Guid id)
+        public async Task<BlogPost?> GetAsync(Guid id)
         {
-            throw new NotImplementedException();
+            return await bloggieDbContext.BlogPosts.Include(x => x.Tags).FirstOrDefaultAsync(x => x.Id == id);
         }
 
-        public Task<BlogPost?> UpdateAsync(BlogPost blogPost)
+        public async Task<BlogPost?> UpdateAsync(BlogPost blogPost)
         {
-            throw new NotImplementedException();
+            var existingBlog = await bloggieDbContext.BlogPosts.Include(x => x.Tags)
+                .FirstOrDefaultAsync(x => x.Id == blogPost.Id);
+
+            if (existingBlog != null)
+            {
+                existingBlog.Id = blogPost.Id;
+                existingBlog.Heading = blogPost.Heading;
+                existingBlog.PageTitle = blogPost.PageTitle;
+                existingBlog.Content = blogPost.Content;
+                existingBlog.ShortDescription = blogPost.ShortDescription;
+                existingBlog.FeaturedImageUrl = blogPost.FeaturedImageUrl;
+                existingBlog.UrlHandle = blogPost.UrlHandle;
+                existingBlog.PublishedDate = blogPost.PublishedDate;
+                existingBlog.Author = blogPost.Author;
+                existingBlog.Visible = blogPost.Visible;
+                existingBlog.Tags = blogPost.Tags;
+
+                await bloggieDbContext.SaveChangesAsync();
+                
+                return existingBlog;
+            }
+
+            return null;
         }
 
         public Task<BlogPost?> DeleteAsync(Guid id) 
